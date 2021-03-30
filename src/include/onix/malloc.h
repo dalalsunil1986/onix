a43bc4f49ea792da765ae90b729a4118258a637b
Steven Kang <kangweibaby@163.com>
2021-03-30 14:04:25 +0800

    add malloc

---
+++ src/include/onix/malloc.h
@@ -0,0 +1,9 @@
+#ifndef ONIX_MALLOC_H
+#define ONIX_MALLOC_H
+
+#include <onix/types.h>
+
+void *_malloc(size_t size);
+void _free(void *ptr);
+
+#endif
---
+++ src/lib/malloc.c
@@ -0,0 +1,10 @@
+#include <onix/malloc.h>
+#include <onix/syscall.h>
+
+void *_malloc(size_t size)
+{
+}
+
+void _free(void *ptr)
+{
+}
