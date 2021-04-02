73b6238fc4197271d536cbea2536e30853a7aea2
Steven Kang <kangweibaby@163.com>
2021-04-02 13:09:36 +0800

    add inode locate

--- src/fs/onix/inode.c
+++ src/fs/onix/inode.c
@@ -1,4 +1,26 @@
 #include <onix/inode.h>
+#include <onix/kernel/harddisk.h>
+
+static void inode_locate(Partition *part, u32 nr, InodePosition *pos)
+{
+    assert(nr < MAX_FILE_PER_PART);
+
+    u32 inode_table_lba = part->super_block->inode_table_lba;
+
+    u32 inode_size = sizeof(Inode);
+    u32 offset_size = nr * inode_size;
+    u32 offset_blocks = offset_size / BLOCK_SIZE;
+    u32 offset_in_block = offset_size % BLOCK_SIZE;
+    u32 left_in_block = BLOCK_SIZE - offset_in_block;
+
+    pos->cross = false;
+    if (left_in_block < inode_size)
+    {
+        pos->cross = true;
+    }
+    pos->sec_lba = inode_table_lba + offset_blocks;
+    pos->offset = offset_in_block;
+}
 
 void inode_init(u32 nr, Inode *inode)
 {
--- src/fs/onix/inode.h
+++ src/fs/onix/inode.h
@@ -4,6 +4,13 @@
 #include <onix/types.h>
 #include <onix/fs.h>
 
+typedef struct InodePosition
+{
+    bool cross;
+    u32 sec_lba;
+    u32 offset;
+} InodePosition;
+
 void inode_init(u32 nr, Inode *inode);
 
 #endif
