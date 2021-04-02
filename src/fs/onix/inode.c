7daaf103cbe883d49b9512b69645801fd853716b
Steven Kang <kangweibaby@163.com>
2021-04-02 13:15:58 +0800

    add inode sync

--- src/fs/onix/inode.c
+++ src/fs/onix/inode.c
@@ -22,6 +22,34 @@ static void inode_locate(Partition *part, u32 nr, InodePosition *pos)
     pos->offset = offset_in_block;
 }
 
+void inode_sync(Partition *part, Inode *inode, void *buf)
+{
+    u32 nr = inode->nr;
+    InodePosition pos;
+    inode_locate(part, nr, &pos);
+
+    assert(pos.sec_lba <= (part->start_lba + part->sec_cnt));
+
+    Inode pure_inode;
+
+    memcpy(&pure_inode, inode, sizeof(Inode));
+
+    pure_inode.open_cnts = 0;
+    pure_inode.write_deny = false;
+    pure_inode.node.prev = NULL;
+    pure_inode.node.next = NULL;
+
+    u8 block_count = 1;
+    if (pos.cross)
+    {
+        block_count = 2;
+    }
+
+    harddisk_read(part->disk, pos.sec_lba, buf, block_count * BLOCK_SECTOR_COUNT);
+    memcpy((buf + pos.offset), &pure_inode, sizeof(Inode));
+    harddisk_write(part->disk, pos.sec_lba, buf, block_count * BLOCK_SECTOR_COUNT);
+}
+
 void inode_init(u32 nr, Inode *inode)
 {
     inode->nr = nr;
