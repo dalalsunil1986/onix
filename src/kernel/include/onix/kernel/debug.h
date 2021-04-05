c498d885a7f1e69b0f3c5f1b3ffb7f0033d6529f
Steven Kang <kangweibaby@163.com>
2021-04-05 19:40:18 +0800

    fix stack check

--- src/kernel/include/onix/kernel/debug.h
+++ src/kernel/include/onix/kernel/debug.h
@@ -5,9 +5,10 @@ void debugk(const char *fmt, ...);
 
 #ifdef ONIX_DEBUG
 #include <onix/kernel/printk.h>
+#include <onix/kernel/assert.h>
 #define BOCHS_MAGIC_BREAKPOINT asm("xchgw %bx, %bx")
 #define DEBUGK(fmt, args...) debugk(__BASE_FILE__, __LINE__, fmt, ##args)
-#define CHECK_STACK (running_task()->magic == TASK_MAGIC);
+#define CHECK_STACK assert(running_task()->magic == TASK_MAGIC);
 #else
 #define BOCHS_MAGIC_BREAKPOINT
 #define DEBUGK(fmt, args...)
