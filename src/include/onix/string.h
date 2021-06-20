f91edfda3e89cdda219bf6a8f4cca8660a814d51
Steven Kang <kangweibaby@163.com>
2021-06-20 17:38:39 +0800

    add string

---
+++ src/include/onix/string.h
@@ -0,0 +1,22 @@
+/* (C) Copyright 2021 Steven;
+* @author: Steven kangweibaby@163.com
+* @date: 2021-06-20
+*/
+
+#ifndef ONIX_STRING_H
+#define ONIX_STRING_H
+
+#include <onix/types.h>
+
+size_t strlen(const char *str);
+
+void memcpy(void *dest, void *src, size_t size);
+void memset(void *dest, char ch, size_t size);
+char *strcpy(char *dest, char *src);
+char *strcat(char *dest, char *src);
+int strcmp(const char *str1, const char *str2);
+
+char *strchr(const char *str, const char ch);
+char *strrchr(const char *str, const char ch);
+
+#endif
